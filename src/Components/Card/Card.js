f3b26f5a9a4cbfe09541a81c339a8e642a2f74f9
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-05-31 23:03:28 +0200

    Ajout d'une animation au chargement de page

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,10 +1,33 @@
 import "./Card.scss"
+import { useTrail, animated } from 'react-spring';
+import {v4 as uuidv4} from "uuid"
 
 
-export default function Card({ children }) {
+export default function Card({ title }) {
+
+  const trail = useTrail( 1, {
+    from: {
+      opacity: 0,
+      x: 50
+    },
+    to: {
+      opacity: 1,
+      x:0
+    }
+  })
+  
   return (
-    <div className='card'>
-        {children}
-    </div>
+    <>
+      {trail.map(cardStyle => {
+        return <animated.div key={uuidv4()} style={cardStyle}>
+
+          <div className="card">
+            <h2>{title}</h2>
+            <a href="/"> Lien vers l'article</a>
+          </div>
+
+        </animated.div>
+      })}
+    </>
   )
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,7 @@
 import "./Home.scss"
 import Card from "../Card/Card"
 import { useSelector, useDispatch } from 'react-redux';
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { getArticles } from "../../redux/articles/articleReducer";
 import {v4 as uuidv4} from "uuid"
 
@@ -20,18 +20,14 @@ export default function Home() {
     }
   }, [])
 
+
   return (
     <>
       <h1 className="home-title">Tous les articles</h1>
 
       <div className="container-cards">
         {articles.map(item => {
-          return (
-              <Card key={uuidv4()}> 
-                <h2> {item.title} </h2>
-                <a href="#"> Lire l'article</a>
-              </Card>
-          )
+          return <Card title={item.title} key={uuidv4()} />
         })}
       </div>
     </>
