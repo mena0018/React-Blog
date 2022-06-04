bd76b485b7bc2272c92a99b1c99af5efabbabd47
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-06-04 18:45:06 +0200

    Ajout d'une page contact

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import "./Contact.scss"
+
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='container-contact'>
+      <h1>Contactez-nous</h1>
+      <p>Par mail : blog-react@gmail.com</p>
+      <p>Par téléphone : xx-xx-xx-xx-xx </p>
+      <p>Rejoignez-nous sur les réseaux :</p>
+
+      <ul>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>YouTube</li>
+      </ul>
+
+    </div>
   )
 }
